1b0d44bc796b0fcb8ea45774d2af0f4dcd6339f9
Walid <walid.benkhelfallah@ynov.com>
2020-06-11 11:11:56 +0200

    pre

---
+++ guide/index.php
@@ -0,0 +1,340 @@
+
+<!DOCTYPE html>
+<html class="no-js"> 
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>LeBonCou</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/animate.css">	
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="stylesheet" id="theme-switch" href="css/style.css">
+
+	<style>
+	.option-toggle {
+		position: absolute;
+		right: 0;
+		top: 0;
+		margin-top: 5px;
+		margin-right: -30px;
+		width: 30px;
+		height: 30px;
+		background: blue;
+		text-align: center;
+		border-top-right-radius: 4px;
+		border-bottom-right-radius: 4px;
+		color: #fff;
+		cursor: pointer;
+		-webkit-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
+		-moz-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
+		-ms-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
+		box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
+	}
+	.option-toggle i {
+		top: 2px;
+		position: relative;
+	}
+	.option-toggle:hover, .option-toggle:focus, .option-toggle:active {
+		color:  #fff;
+		text-decoration: none;
+		outline: none;
+	}
+	span{
+		color: greenyellow;
+	}
+	</style>
+
+
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	</head>
+
+
+	<body>
+	
+
+	<div class="fh5co-loader"></div>
+	
+	<div id="fh5co-page">
+		<section id="fh5co-header">
+			<div class="container">
+				<nav role="navigation">
+					<ul class="pull-left left-menu">
+						<li><a href="about.html">About</a></li>
+						<li><a href="tour.html">Annonce</a></li>
+						<li><a href="pricing.html">Recherche</a></li>
+					</ul>
+					<h1 id="fh5co-logo"><a href="index.html">LeBonCou<span>.</span></a></h1>
+					<ul class="pull-right right-menu">
+						<li><a href="#">Login</a></li>
+						<li class="fh5co-cta-btn"><a href="#">Sign up</a></li>
+					</ul>
+				</nav>
+			</div>
+		</section>
+
+		<section id="fh5co-hero" class="js-fullheight" style="background-image: url(images/full_image_1.jpg);" data-next="yes">
+			<div class="fh5co-overlay"></div>
+			<div class="container">
+				<div class="fh5co-intro js-fullheight">
+					<div class="fh5co-intro-text">
+						<div class="fh5co-left-position">
+							<h2 class="animate-box">Tout vos le coup sur LeBonCou<span>.</span></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-learn-more animate-box">
+				<a href="#" class="scroll-btn">
+					<span class="text">Voir les Annonces</span>
+					<span class="arrow"><i class="icon-chevron-down"></i></span>
+				</a>
+			</div>
+		</section>
+
+
+		<section id="fh5co-projects">
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<h2 class="fh5co-lead animate-box">ANNONCES</h2>
+						<p class="fh5co-sub-lead animate-box">Une Lambo ? un cheval ou maison ? tu trouvera tout ici.</Label> </p>
+					</div>
+				</div>
+				<div class="row">
+					
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/lambo.jpg" class="fh5co-project-item image-popup">
+							<img src="images/lambo.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>Bête de voiture pas chère</h2>
+								<p>Facile a prendre en main, confortable et belle. 12€</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/cacahuete.jpg" class="fh5co-project-item image-popup">
+							<img src="images/cacahuete.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>Poignée de Cacahuète</h2>
+								<p>Poignée de 15 cacahuète succulente.
+									0,99€/cacahuète </p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/maison.jpg" class="fh5co-project-item image-popup">
+							<img src="images/maison.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>Cabane</h2>
+								<p>Cabane plutôt cool sur le Seine 
+									27€
+								</p>
+							</div>
+					</div>
+
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/ordi.jpg" class="fh5co-project-item image-popup">
+							<img src="images/ordi.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>Ordinateur</h2>
+								<p>Ordinateur doté d'une puissance inimaginable.		
+								7€
+								</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/img_5.jpg" class="fh5co-project-item image-popup">
+							<img src="images/img_5.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>A Big Wave of the Blue Sea</h2>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
+						<a href="images/img_6.jpg" class="fh5co-project-item image-popup">
+							<img src="images/img_6.jpg" alt="Image" class="img-responsive">
+							<div class="fh5co-text">
+								<h2>Foggy Pine Trees</h2>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							</div>
+						</a>
+					</div>
+					
+					
+				</div>
+			</div>
+		</section>
+
+		<section id="fh5co-testimonials">
+			<div class="container">
+				<div class="row row-bottom-padded-sm">
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<h2 class="fh5co-lead animate-box">Rejoins les 1 million d'utilisateur de LeBonCou<span>.</span> </h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center animate-box">
+						<div class="flexslider">
+					  		<ul class="slides">
+							   <li>
+							      <blockquote>
+							      	<p>&ldquo;Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn’t really do it, they just saw something. It seemed obvious to them after a while.&rdquo;</p>
+							      </blockquote>
+							   </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="fh5co-subscribe">
+			<div class="container">
+		
+				<h3 class="animate-box"><label for="email">Rechercher une annonce</label></h3>
+				<form action="#" method="post" class="animate-box">
+					<i class="fh5co-icon icon-paper-plane"></i>
+					<input type="email" class="form-control" placeholder="Rechercher" id="email" name="email">
+					<input type="submit" value="Send" class="btn btn-primary">
+				</form>
+
+			</div>
+		</section>
+
+		<footer id="fh5co-footer">
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Company</h3>
+							<ul class="fh5co-links">
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Careers</a></li>
+								<li><a href="#">Feature Tour</a></li>
+								<li><a href="#">Pricing</a></li>
+								<li><a href="#">Team</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Contact Us</h3>
+							<p>
+								<a href="mailto:info@freehtml5.co">LeBonCou@coucou.com</a> <br>
+								La rue la vraie <br>
+								france <br>
+								<a href="tel:+123456789">+333333333333</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Follow Us</h3>
+							<ul class="fh5co-social">
+								<li><a href="#"><i class="icon-twitter">twitter:</i></a></li>
+								<li><a href="#"><i class="icon-instagram">insta:</i></a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+				
+			</div>
+			<div class="fh5co-copyright animate-box">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p class="fh5co-left"><small>&copy; 2016 <a href="index.html">Guide</a> free html5. All Rights Reserved.</small></p>
+							<p class="fh5co-right"><small class="fh5co-right">Designed by <a href="http://freehtml5.co" target="_blank">FREEHTML5.co</a> Demo Images: <a href="http://unsplash.com" target="_blank">Unsplash</a></small></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+
+	
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<script src="js/jquery.style.switcher.js"></script>
+	<script>
+		$(function(){
+			$('#colour-variations ul').styleSwitcher({
+				defaultThemeId: 'theme-switch',
+				hasPreview: false,
+				cookie: {
+		          	expires: 30,
+		          	isManagingLoad: true
+		      	}
+			});	
+			$('.option-toggle').click(function() {
+				$('#colour-variations').toggleClass('sleep');
+			});
+		});
+	</script>
+
+	<script src="js/main.js"></script>
+
+
+	<script src="js/jquery.cookie.js"></script>
+	<script>
+		$(function(){
+
+			if ( $.cookie('layoutCookie') != '' ) {
+				$('body').addClass($.cookie('layoutCookie'));
+			}
+
+			$('a[data-layout="boxed"]').click(function(event){
+				event.preventDefault();
+				$.cookie('layoutCookie', 'boxed', { expires: 7, path: '/'});
+				$('body').addClass($.cookie('layoutCookie')); 
+
+			$('a[data-layout="wide"]').click(function(event){
+				event.preventDefault();
+				$('body').removeClass($.cookie('layoutCookie')); 
+				$.removeCookie('layoutCookie', { path: '/' }); 
+			});
+		});
+	</script>
+	
+
+	</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,331 +1,85 @@
-
 <!DOCTYPE html>
-<html class="no-js"> 
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>LeBonCou</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
+<html lang="en">
 
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+<head>
 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-	<link rel="shortcut icon" href="favicon.ico">
-	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,600,400italic,700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/animate.css">	
-	<link rel="stylesheet" href="css/magnific-popup.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
+  <title>LeBonCou</title>
 
-	<link rel="stylesheet" href="css/style.css">
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-	<link rel="stylesheet" id="theme-switch" href="css/style.css">
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 
-	<style>
-	.option-toggle {
-		position: absolute;
-		right: 0;
-		top: 0;
-		margin-top: 5px;
-		margin-right: -30px;
-		width: 30px;
-		height: 30px;
-		background: blue;
-		text-align: center;
-		border-top-right-radius: 4px;
-		border-bottom-right-radius: 4px;
-		color: #fff;
-		cursor: pointer;
-		-webkit-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
-		-moz-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
-		-ms-box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
-		box-shadow: 0 0 9px 0 rgba(0,0,0,.1);
-	}
-	.option-toggle i {
-		top: 2px;
-		position: relative;
-	}
-	.option-toggle:hover, .option-toggle:focus, .option-toggle:active {
-		color:  #fff;
-		text-decoration: none;
-		outline: none;
-	}
-	span{
-		color: greenyellow;
-	}
-	</style>
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
 
+</head>
 
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	</head>
+<body id="page-top">
 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#annonce">Annonce</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="recherche.php">Recherche</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#profil">Profil</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-	<body>
-	
+  <div class="container-fluid p-0">
 
-	<div class="fh5co-loader"></div>
-	
-	<div id="fh5co-page">
-		<section id="fh5co-header">
-			<div class="container">
-				<nav role="navigation">
-					<ul class="pull-left left-menu">
-						<li><a href="about.html">About</a></li>
-						<li><a href="tour.html">Annonce</a></li>
-						<li><a href="pricing.html">Recherche</a></li>
-					</ul>
-					<h1 id="fh5co-logo"><a href="index.html">LeBonCou<span>.</span></a></h1>
-					<ul class="pull-right right-menu">
-						<li><a href="#">Login</a></li>
-						<li class="fh5co-cta-btn"><a href="#">Sign up</a></li>
-					</ul>
-				</nav>
-			</div>
-		</section>
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 href="titre" class="mb-0">LeBonCou</h1>
+      </div>
+    </section>
 
-		<section id="fh5co-hero" class="js-fullheight" style="background-image: url(images/full_image_1.jpg);" data-next="yes">
-			<div class="fh5co-overlay"></div>
-			<div class="container">
-				<div class="fh5co-intro js-fullheight">
-					<div class="fh5co-intro-text">
-						<div class="fh5co-left-position">
-							<h2 class="animate-box">Tout vos le coup sur LeBonCou<span>.</span></h2>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="fh5co-learn-more animate-box">
-				<a href="#" class="scroll-btn">
-					<span class="text">Voir les Annonces</span>
-					<span class="arrow"><i class="icon-chevron-down"></i></span>
-				</a>
-			</div>
-		</section>
+    <hr class="m-0">
 
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="recherche">
+      <div class="w-100">
+        <h2 class="mb-5">Recherche</h2>
 
-		<section id="fh5co-projects">
-			<div class="container">
-				<div class="row row-bottom-padded-md">
-					<div class="col-md-6 col-md-offset-3 text-center">
-						<h2 class="fh5co-lead animate-box">ANNONCES</h2>
-						<p class="fh5co-sub-lead animate-box">Une Lambo ? un cheval ou maison ? tu trouvera tout ici.</Label> </p>
-					</div>
-				</div>
-				<div class="row">
-					
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/lambo.jpg" class="fh5co-project-item image-popup">
-							<img src="images/lambo.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>Bête de voiture pas chère</h2>
-								<p>Facile a prendre en main, confortable et belle. 12€</p>
-							</div>
-						</a>
-					</div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3> mettre formulaire de recherche</h3>
+    </section>
 
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/cacahuete.jpg" class="fh5co-project-item image-popup">
-							<img src="images/cacahuete.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>Poignée de Cacahuète</h2>
-								<p>Poignée de 15 cacahuète succulente.
-									0,99€/cacahuète </p>
-							</div>
-						</a>
-					</div>
+    
 
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/maison.jpg" class="fh5co-project-item image-popup">
-							<img src="images/maison.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>Cabane</h2>
-								<p>Cabane plutôt cool sur le Seine 
-									27€
-								</p>
-							</div>
-					</div>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/ordi.jpg" class="fh5co-project-item image-popup">
-							<img src="images/ordi.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>Ordinateur</h2>
-								<p>Ordinateur doté d'une puissance inimaginable.		
-								7€
-								</p>
-							</div>
-						</a>
-					</div>
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/img_5.jpg" class="fh5co-project-item image-popup">
-							<img src="images/img_5.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>A Big Wave of the Blue Sea</h2>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							</div>
-						</a>
-					</div>
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
 
-					<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-						<a href="images/img_6.jpg" class="fh5co-project-item image-popup">
-							<img src="images/img_6.jpg" alt="Image" class="img-responsive">
-							<div class="fh5co-text">
-								<h2>Foggy Pine Trees</h2>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							</div>
-						</a>
-					</div>
-					
-					
-				</div>
-			</div>
-		</section>
+</body>
 
-		<section id="fh5co-testimonials">
-			<div class="container">
-				<div class="row row-bottom-padded-sm">
-					<div class="col-md-6 col-md-offset-3 text-center">
-						<h2 class="fh5co-lead animate-box">yo les 1 million d'utilisateur de LeBonCou<span>.</span> </h2>
-					</div>
-				</div>					
-				</div>
-			</div>
-		</section>
-
-		<footer id="fh5co-footer">
-			<div class="container">
-				<div class="row row-bottom-padded-md">
-					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-						<div class="fh5co-footer-widget">
-							<h3>Company</h3>
-							<ul class="fh5co-links">
-								<li><a href="#">About Us</a></li>
-								<li><a href="#">Careers</a></li>
-								<li><a href="#">Feature Tour</a></li>
-								<li><a href="#">Pricing</a></li>
-								<li><a href="#">Team</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-						<div class="fh5co-footer-widget">
-							<h3>Support</h3>
-							<ul class="fh5co-links">
-								<li><a href="#">Knowledge Base</a></li>
-								<li><a href="#">24/7 Call Support</a></li>
-								<li><a href="#">Video Demos</a></li>
-								<li><a href="#">Terms of Use</a></li>
-								<li><a href="#">Privacy Policy</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-						<div class="fh5co-footer-widget">
-							<h3>Contact Us</h3>
-							<p>
-								<a href="mailto:info@freehtml5.co">info@freehtml5.co</a> <br>
-								198 West 21th Street, <br>
-								Suite 721 New York NY 10016 <br>
-								<a href="tel:+123456789">+12 34  5677 89</a>
-							</p>
-						</div>
-					</div>
-
-					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
-						<div class="fh5co-footer-widget">
-							<h3>Follow Us</h3>
-							<ul class="fh5co-social">
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-google-plus"></i></a></li>
-								<li><a href="#"><i class="icon-instagram"></i></a></li>
-								<li><a href="#"><i class="icon-youtube-play"></i></a></li>
-							</ul>
-						</div>
-					</div>
-
-				</div>
-				
-			</div>
-			<div class="fh5co-copyright animate-box">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<p class="fh5co-left"><small>&copy; 2016 <a href="index.html">Guide</a> free html5. All Rights Reserved.</small></p>
-							<p class="fh5co-right"><small class="fh5co-right">Designed by <a href="http://freehtml5.co" target="_blank">FREEHTML5.co</a> Demo Images: <a href="http://unsplash.com" target="_blank">Unsplash</a></small></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</div>
-
-
-	
-
-	<script src="js/jquery.min.js"></script>
-	<script src="js/jquery.easing.1.3.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.waypoints.min.js"></script>
-	<script src="js/jquery.flexslider-min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/magnific-popup-options.js"></script>
-
-	<script src="js/jquery.style.switcher.js"></script>
-	<script>
-		$(function(){
-			$('#colour-variations ul').styleSwitcher({
-				defaultThemeId: 'theme-switch',
-				hasPreview: false,
-				cookie: {
-		          	expires: 30,
-		          	isManagingLoad: true
-		      	}
-			});	
-			$('.option-toggle').click(function() {
-				$('#colour-variations').toggleClass('sleep');
-			});
-		});
-	</script>
-
-	<script src="js/main.js"></script>
-
-
-	<script src="js/jquery.cookie.js"></script>
-	<script>
-		$(function(){
-
-			if ( $.cookie('layoutCookie') != '' ) {
-				$('body').addClass($.cookie('layoutCookie'));
-			}
-
-			$('a[data-layout="boxed"]').click(function(event){
-				event.preventDefault();
-				$.cookie('layoutCookie', 'boxed', { expires: 7, path: '/'});
-				$('body').addClass($.cookie('layoutCookie')); 
-
-			$('a[data-layout="wide"]').click(function(event){
-				event.preventDefault();
-				$('body').removeClass($.cookie('layoutCookie')); 
-				$.removeCookie('layoutCookie', { path: '/' }); 
-			});
-		});
-	</script>
-	
-
-	</body>
 </html>
-
